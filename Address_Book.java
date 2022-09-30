af86c247cb1c7e883587fd898d32ebb37fa83b2f
amod1811 <ammch101@gmail.com>
2022-09-30 16:43:39 +0530

    In this program write Add Method to add the contact in adderss book

--- Address_Book.java
+++ Address_Book.java
@@ -10,7 +10,22 @@ public class Address_Book {
 	private String city;
 	private String state;
 
-	
+	public void addcontact() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the First name");
+		firstname = sc.next();
+		System.out.println("Enter the Last Name");
+		lastname = sc.next();
+		System.out.println("Enter the Mobile Number");
+		mobilenumber = sc.nextInt();
+		System.out.println("Enter the Email Address");
+		email = sc.next();
+		System.out.println("Enter the City");
+		city = sc.next();
+		System.out.println("Enter the State");
+		state = sc.next();
+
+	}
 
 	
 
