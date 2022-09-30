dc6caae3111026899998e78ed4612f44879701b6
amod1811 <ammch101@gmail.com>
2022-09-30 16:41:28 +0530

    Added program use main method to call the proram

--- AddressBook_Main.java
+++ AddressBook_Main.java
@@ -3,8 +3,34 @@ package bl.com.addressbooksystem200;
 import java.util.Scanner;
 
 public class AddressBook_Main {
+	Address_Book addressbook = new Address_Book();
+	Scanner sc = new Scanner(System.in);
+
+	public void getmenu() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Menu of Address Book");
+		boolean check = false;
+		do {
+			System.out.println("Enter Your Choice" + "\n1. Add Contact");
+			int option = sc.nextInt();
+			switch (option) {
+			case 1:
+				addressbook.addcontact();
+				break;
+			default:
+				System.out.println("Enter the valid choice");
+			}
+		} while (!check);
+
+	}
+
+	
 
 	public static void main(String[] args) {
 		System.out.println("Welcome To Address Book System");
+		AddressBook_Main addressBook = new AddressBook_Main();
+		addressBook.getmenu();
+		Address_Book addressbook = new Address_Book();
+		addressbook.addcontact();
 	}
 }
