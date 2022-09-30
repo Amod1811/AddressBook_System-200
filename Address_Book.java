d27c569b44352354c352546889c4193e38b58ee2
amod1811 <ammch101@gmail.com>
2022-09-30 16:32:30 +0530

    Added program to create contact

---
+++ Address_Book.java
@@ -0,0 +1,17 @@
+package bl.com.addressbooksystem200;
+
+import java.util.Scanner;
+
+public class Address_Book {
+	private String firstname;
+	private String lastname;
+	private int mobilenumber;
+	private String email;
+	private String city;
+	private String state;
+
+	
+
+	
+
+}
