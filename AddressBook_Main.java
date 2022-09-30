02dbf2c8f01d525f0d09ece2fb4deb40fd656320
amod1811 <ammch101@gmail.com>
2022-09-30 16:30:21 +0530

    Added program to main method

---
+++ AddressBook_Main.java
@@ -0,0 +1,10 @@
+package bl.com.addressbooksystem200;
+
+import java.util.Scanner;
+
+public class AddressBook_Main {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome To Address Book System");
+	}
+}
