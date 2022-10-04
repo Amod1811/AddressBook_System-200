404053d1e47c83b3c5c8f745f011d309e9ce9ad7
amod1811 <ammch101@gmail.com>
2022-10-04 15:09:31 +0530

    Added the multiple person contact

--- AddressBook_Main.java
+++ AddressBook_Main.java
@@ -11,21 +11,22 @@ public class AddressBook_Main {
 		System.out.println("Menu of Address Book");
 		boolean check = false;
 		do {
-			System.out.println("Enter Your Choice" + "\n1. Add Contact"
-					+ "\n2. Display Contact"
-					+ "\n3. Edit Contact"
+			System.out.println("Enter Your Choice" + "\n1. Add Contact" + "\n2. Display Contact" + "\n3. Edit Contact"
 					+ "\n4. Delete Contact");
 			int option = sc.nextInt();
 			switch (option) {
 			case 1:
 				addressbook.addcontact();
 				break;
-			case 2: addressbook.displayContact();
+			case 2:
+				addressbook.displayContact();
 				break;
-			case 3:  addressbook.editContact();
-			break;
-			case 4:addressbook.deletecontact();
-			break;
+			case 3:
+				addressbook.editContact();
+				break;
+			case 4:
+				addressbook.deletecontact();
+				break;
 			default:
 				System.out.println("Enter the valid choice");
 			}
@@ -33,13 +34,9 @@ public class AddressBook_Main {
 
 	}
 
-	
-
 	public static void main(String[] args) {
 		System.out.println("Welcome To Address Book System");
 		AddressBook_Main addressBook = new AddressBook_Main();
 		addressBook.getmenu();
-		Address_Book addressbook = new Address_Book();
-		addressbook.addcontact();
 	}
 }
